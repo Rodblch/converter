cdf18b993178c4251b1119f4b219d16f38b14826
Rodblch <76397201+Rodblch@users.noreply.github.com>
2020-12-22 19:13:36 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,32 +1,5 @@
 #include <stdio.h>
 
-#define BYN 1.0
-#define RUB 0.034
-#define USD 2.52
-#define EUR 3.08
-
-char currency_type[4][20] = {"EUR", "USD", "RUB", "BYN"};
-float currency[4] = { 0 };
-
-/* Что бы избежать повторения одного и того же кода лучше перенести информацию в отдельную функцию */
-void display_currency(void) {
-    for(int i = 0; i < 4; ++i) {
-        printf("%.2f %s\n", currency[i], currency_type[i]);
-    }
-}
-
-int main (void) {
-
-    int input;
-    float num;
-
-    do {
-        printf("Какую валюту использвать?\n");
-        printf("1. BYN\n");
-        printf("2. RUB\n");
-        printf("3. USD\n");
-#include <stdio.h>
-
 char currency_type[4][20] = {"EUR", "USD", "RUB", "BYN"};
 float currency[4] = { 0 };
 
